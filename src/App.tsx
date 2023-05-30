7226ebedbaab9bb7de263ce16b67a4043d06b2b8
David Rivera Morales <driveramorales46@gmail.com>
2023-05-30 00:32:45 -0600

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,55 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { Transition } from "@headlessui/react";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
+    <div className="flex h-screen bg-gray-100">
+      <Transition
+        show={isOpen}
+        enter="transition-all duration-500"
+        enterFrom="opacity-0 transform translate-x-[-100%]"
+        enterTo="opacity-100 transform translate-x-0"
+        leave="transition-all duration-500"
+        leaveFrom="opacity-100 transform translate-x-0"
+        leaveTo="opacity-0 transform translate-x-[-100%]"
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
+        <div className="w-1/2 h-full bg-white overflow-auto p-4">
+          <pre className="text-sm text-gray-800"></pre>
+        </div>
+      </Transition>
 
-      <p>{greetMsg}</p>
+      <Transition
+        show={!isOpen}
+        enter="transition-all duration-500"
+        enterFrom="opacity-0 transform translate-x-[100%]"
+        enterTo="opacity-100 transform translate-x-0"
+        leave="transition-all duration-500"
+        leaveFrom="opacity-100 transform translate-x-0"
+        leaveTo="opacity-0 transform translate-x-[100%]"
+      >
+        <div className="w-1/2 h-full bg-white overflow-auto p-4">
+          <div className="text-sm text-gray-800">
+            {/* Your LaTeX output goes here */}
+          </div>
+        </div>
+      </Transition>
+
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="absolute top-4 right-4 p-2 bg-blue-500 text-white rounded"
+      >
+        Toggle View
+      </button>
     </div>
   );
 }
