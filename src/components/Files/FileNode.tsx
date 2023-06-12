482cd8f7557851061e50e11dab077e34cb887ae2
David Rivera Morales <driveramorales46@gmail.com>
2023-06-12 08:02:26 -0600

    add file structure

--- src/components/Files/FileNode.tsx
+++ src/components/Files/FileNode.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/outline";
 export interface ITreeNode {
   id: number;
   name: string;
@@ -12,6 +12,7 @@ interface TreeNodeProps {
   level: number;
   selectedNode: ITreeNode | null;
   onNodeClick: (node: ITreeNode) => void;
+  onNodeAdd: (parentId: number, node: Omit<ITreeNode, "id">) => void;
 }
 
 export const TreeNode: React.FC<TreeNodeProps> = ({
@@ -19,21 +20,69 @@ export const TreeNode: React.FC<TreeNodeProps> = ({
   level,
   selectedNode,
   onNodeClick,
-}) => (
-  <div
-    className={`pl-${level} p-2 cursor-pointer hover:bg-blue-100 rounded 
+  onNodeAdd,
+}) => {
+  const [adding, setAdding] = useState(false);
+  const [newNodeName, setNewNodeName] = useState("");
+  const [newNodeType, setNewNodeType] = useState<"file" | "folder">("file");
+
+  const handleAddClick = () => {
+    setAdding(true);
+  };
+
+  const handleAddConfirm = () => {
+    onNodeAdd(node.id, {
+      name: newNodeName,
+      isFolder: newNodeType === "folder",
+      children: newNodeType === "folder" ? [] : undefined,
+    });
+    setNewNodeName("");
+    setAdding(false);
+  };
+
+  return (
+    <div
+      className={`pl-${level} p-2 cursor-pointer hover:bg-blue-100 rounded 
                 ${selectedNode?.id === node.id ? "shadow-lg bg-blue-100" : ""}`}
-    onClick={() => onNodeClick(node)}
-  >
-    {node.isFolder ? <b className="text-blue-500">{node.name}</b> : node.name}
-    {node.children?.map((childNode) => (
-      <TreeNode
-        key={childNode.id}
-        node={childNode}
-        level={level + 1}
-        selectedNode={selectedNode}
-        onNodeClick={onNodeClick}
-      />
-    ))}
-  </div>
-);
+      onClick={() => onNodeClick(node)}
+    >
+      {node.isFolder ? <b className="text-blue-500">{node.name}</b> : node.name}
+      {node.isFolder && (
+        <PlusIcon
+          className="h-5 w-5 ml-2 text-blue-500"
+          onClick={handleAddClick}
+        />
+      )}
+      {adding && (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={newNodeName}
+            onChange={(e) => setNewNodeName(e.target.value)}
+          />
+          <select
+            value={newNodeType}
+            onChange={(e) =>
+              setNewNodeType(e.target.value as "file" | "folder")
+            }
+          >
+            <option value="file">File</option>
+            <option value="folder">Folder</option>
+          </select>
+          <button onClick={handleAddConfirm}>Add</button>
+        </div>
+      )}
+      {node.children?.map((childNode) => (
+        <TreeNode
+          key={childNode.id}
+          node={childNode}
+          level={level + 1}
+          selectedNode={selectedNode}
+          onNodeClick={onNodeClick}
+          onNodeAdd={onNodeAdd}
+        />
+      ))}
+    </div>
+  );
+};
--- src/components/Files/FileTree.tsx
+++ src/components/Files/FileTree.tsx
@@ -16,21 +16,39 @@ const initialTree: ITreeNode = {
     },
   ],
 };
-
 const TreeFile: React.FC = () => {
   const [selectedNode, setSelectedNode] = useState<ITreeNode | null>(null);
+  const [tree, setTree] = useState(initialTree);
 
   const handleNodeClick = (node: ITreeNode) => {
     setSelectedNode(node);
   };
 
+  const handleNodeAdd = (parentId: number, node: Omit<ITreeNode, "id">) => {
+    const newNode = { id: Math.random(), ...node };
+    const addNode = (tree: ITreeNode): ITreeNode => {
+      if (tree.id === parentId) {
+        return {
+          ...tree,
+          children: [...(tree.children ?? []), newNode],
+        };
+      }
+      return {
+        ...tree,
+        children: tree.children?.map(addNode),
+      };
+    };
+    setTree(addNode(tree));
+  };
+
   return (
     <div className="bg-blue-100 rounded shadow min-h-screen p-4">
       <TreeNode
-        node={initialTree}
+        node={tree}
         level={0}
         selectedNode={selectedNode}
         onNodeClick={handleNodeClick}
+        onNodeAdd={handleNodeAdd}
       />
       {selectedNode && (
         <div className="mt-4 text-blue-500">Selected: {selectedNode.name}</div>
